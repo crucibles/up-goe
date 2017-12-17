f1c5cc12b24a25102a2d2a87bb86d637bf8686f5
AJ Ruth Sumandang <ahsumandang@up.edu.ph>
2017-12-18 00:48:11 +0800

    added general select-course page

---
+++ src/app/gen-selcourse/gen-selcourse.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GenSelcourseComponent } from './gen-selcourse.component';
+
+describe('GenSelcourseComponent', () => {
+  let component: GenSelcourseComponent;
+  let fixture: ComponentFixture<GenSelcourseComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GenSelcourseComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GenSelcourseComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/gen-selcourse/gen-selcourse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gen-selcourse',
+  templateUrl: './gen-selcourse.component.html',
+  styleUrls: ['./gen-selcourse.component.css']
+})
+export class GenSelcourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
