7cdfaead66c533634e7e51257fb88bea3c534eb2
asumandang <ahsumandang@up.edu.ph>
2018-05-20 05:27:52 +0800

    added teacher's specific pages namely Specific Character, My-Course, Specific News, Specific Profile, Specific QuestMap, Specific Side Tab, Specific Topnavbar (with routing and module declaration)

---
+++ src/app/teacher/specific/index.ts
@@ -0,0 +1,3 @@
+
+export * from './specific-topnavbar/specific-topnavbar.component';
+export * from './specific-sidetab/specific-sidetab.component';
---
+++ src/app/teacher/specific/pages/specific-character/specific-character.component.ts
@@ -0,0 +1,84 @@
+//Core Imports
+import {
+	Component,
+	OnInit,
+	HostListener
+} from '@angular/core';
+
+import {
+	ActivatedRoute
+} from '@angular/router';
+
+//Application Imports
+import {
+	Section
+} from 'shared/models';
+
+import {
+	PageService,
+	SectionService
+} from 'shared/services';
+
+@Component({
+	selector: 'app-specific-character',
+	templateUrl: './specific-character.component.html',
+	styleUrls: ['./specific-character.component.css']
+})
+export class SpecificCharacterComponent implements OnInit {
+
+	private currentSection: Section;
+
+	private ctx: any = {
+		isImage: true
+	};
+
+	//for collapsible equipments/consummables
+	windowWidth: number = window.innerWidth;
+	isShowEqCon: boolean = this.windowWidth <= 800? false: true;
+
+	//if screen size changes it'll update
+	@HostListener('window:resize', ['$event'])
+	resize(event) {
+		this.checkSize();
+	}
+
+	constructor(
+		private pageService: PageService,
+		private sectionService: SectionService,
+		private route: ActivatedRoute
+	) { }
+
+	ngOnInit() {
+		this.setDefault();
+		this.getCurrentSection();
+	}
+
+	/**
+	 * Sets all the default less-related functions/properties of the component
+	 */
+	setDefault() {
+		this.pageService.isProfilePage(false);
+	}
+
+	/**
+	 * Obtains the user's navigated section
+	 * @description Obtains the current section and stores it into 'currentSection' variable
+	 */
+	getCurrentSection() {
+		this.route.paramMap.subscribe(params => {
+			let sectionId = params.get('sectionId');
+		});
+	}
+
+	checkSize() {
+		this.windowWidth = window.innerWidth;
+		if (this.windowWidth <= 800) {
+			this.isShowEqCon = false;
+			console.log("less than or equal: " + this.windowWidth);
+		} else {
+			this.isShowEqCon = true;
+			console.log("greater: " + this.windowWidth);
+		}
+	}
+
+}
